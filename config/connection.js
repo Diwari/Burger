84737e40f628a1d980bba2bd282fb860ab9fd63f
unknown <mrtoryan@outlook.com>
2019-03-25 07:57:21 -0400

    added Heroku jawsdb code to connections

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,15 @@
 let mysql = require("mysql");
 
-let connection = mysql.createConnection({
+let connection;
+if(process.env.JAWSBD_URL){
+  connection = mysql.createConnection(process.env.JAWSBD_URL);
+} else{
   host: "localhost",
   port: 3306,
   user: "root",
   password: "M!sterT21",
   database: "burgers_db"
-});
-
+}
 connection.connect(function(err){
   if (err) {
     console.error("error connection: " + err.stack);
